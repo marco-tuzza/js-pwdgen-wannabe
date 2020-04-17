2f761e0816c4f483d1de7969ce13d28275315527
Marco Tuzza <marcotuzza@hotmail.it>
2020-04-17 17:56:02 +0200

    Esercizio completato (manca l'abbellimento col CSS)

---
+++ main.js
@@ -0,0 +1,10 @@
+// Chiediamo il nome all'utente
+var nome = prompt('Inserisci il tuo nome (senza cognome)');
+// Chiediamo il cognome
+var cognome = prompt('Inserisci il tuo cognome');
+// Chiediamo il colore preferito
+var colore = prompt('Inserisci il tuo colore preferito');
+// Inseriamo noi stessi l'anno corrente
+var anno_corrente = 2020;
+// Generiamo e mostriamo la password all'utente
+document.writeln ('Ecco la tua password: ' + nome + cognome + colore + anno_corrente);
